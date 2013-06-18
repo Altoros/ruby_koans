e4d4e88542f6dcd90971a7e5e09db90eea48e6c0
Julio Lucero <poramo@gmail.com>
2013-06-18 15:15:13 -0300

    string

---
+++ koans/about_strings.rb
@@ -0,0 +1,153 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutStrings < Neo::Koan
+  def test_double_quoted_strings_are_strings
+    string = "Hello, World"
+    assert_equal true, string.is_a?(String)
+  end
+
+  def test_single_quoted_strings_are_also_strings
+    string = 'Goodbye, World'
+
+    # Test is string is a String object.
+    assert_equal true, __
+  end
+
+  def test_use_single_quotes_to_create_string_with_double_quotes
+    string = 'He said, "Go Away."'
+    assert_equal __, string
+  end
+
+  def test_use_double_quotes_to_create_strings_with_single_quotes
+    string = "Don't"
+    assert_equal __, string
+  end
+
+  def test_use_backslash_for_those_hard_cases
+    a = "He said, \"Don't\""
+    b = 'He said, "Don\'t"'
+    assert_equal __, a == b
+  end
+
+  # def test_use_flexible_quoting_to_handle_really_hard_cases
+  #   a = %(flexible quotes can handle both ' and " characters)
+  #   b = %!flexible quotes can handle both ' and " characters!
+  #   c = %{flexible quotes can handle both ' and " characters}
+  #   assert_equal __, a == b
+  #   assert_equal __, a == c
+  # end
+
+  def test_plus_will_concatenate_two_strings
+    string = "Hello, " + "World"
+    assert_equal __, string
+  end
+
+  def test_plus_concatenation_will_leave_the_original_strings_unmodified
+    hi = "Hello, "
+    there = "World"
+    string = hi + there
+    assert_equal __, hi
+    assert_equal __, there
+  end
+
+  def test_plus_equals_will_concatenate_to_the_end_of_a_string
+    hi = "Hello, "
+    there = "World"
+    hi += there
+    assert_equal __, hi
+  end
+
+  # def test_plus_equals_also_will_leave_the_original_string_unmodified
+  #   original_string = "Hello, "
+  #   hi = original_string
+  #   there = "World"
+  #   hi += there
+  #   assert_equal __, original_string
+  # end
+
+  def test_the_shovel_operator_will_also_append_content_to_a_string
+    hi = "Hello, "
+    there = "World"
+    hi << there
+    assert_equal __, hi
+    assert_equal __, there
+  end
+
+  def test_the_shovel_operator_modifies_the_original_string
+    original_string = "Hello, "
+    hi = original_string
+    there = "World"
+    hi << there
+    assert_equal __, original_string
+
+    # THINK ABOUT IT:
+    #
+    # Ruby programmers tend to favor the shovel operator (<<) over the
+    # plus equals operator (+=) when building up strings.  Why?
+  end
+
+  def test_double_quoted_strings_interpolate_variables
+    value = 123
+    string = "The value is #{value}"
+    assert_equal __, string
+  end
+
+  def test_single_quoted_strings_do_not_interpolate
+    value = 123
+    string = 'The value is #{value}'
+    assert_equal __, string
+  end
+
+  def test_any_ruby_expression_may_be_interpolated
+    string = "The square root of 5 is #{Math.sqrt(5)}"
+    assert_equal __, string
+  end
+
+  # def test_you_can_get_a_substring_from_a_string
+  #   string = "Bacon, lettuce and tomato"
+  #   assert_equal __, string[7,3]
+  #   assert_equal __, string[7..9]
+  # end
+
+  # in_ruby_version("1.8") do
+  #   def test_in_ruby_1_8_single_characters_are_represented_by_integers
+  #     assert_equal __, ?a
+  #     assert_equal __, ?a == 97
+
+  #     assert_equal __, ?b == (?a + 1)
+  #   end
+  # end
+
+  # in_ruby_version("1.9") do
+  #   def test_in_ruby_1_9_single_characters_are_represented_by_strings
+  #     assert_equal __, ?a
+  #     assert_equal __, ?a == 97
+  #   end
+  # end
+
+  def test_strings_can_be_split
+    string = "Sausage Egg Cheese"
+    words = string.split
+    assert_equal [__, __, __], words
+  end
+
+  def test_strings_can_be_split_with_different_patterns
+    string = "the:rain:in:spain"
+
+    # Use slitp(/something/)
+    assert_equal [__, __, __, __], words
+  end
+
+  def test_strings_can_be_joined
+    words = ["Now", "is", "the", "time"]
+    assert_equal __, words.join(" ")
+  end
+
+  # def test_strings_are_unique_objects
+  #   a = "a string"
+  #   b = "a string"
+
+  #   assert_equal __, a           == b
+  #   assert_equal __, a.object_id == b.object_id
+  # end
+end
